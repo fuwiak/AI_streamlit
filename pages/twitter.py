7b8d3e4d38b75bd6962ded261d64ca3752bba536
Леонид Каретников <poczta130@gmail.com>
2022-11-09 02:29:45 +0000

    twitter.py

--- pages/twitter.py
+++ pages/twitter.py
@@ -6,13 +6,10 @@ import pandas as pd
 import twint
 from collections import Counter
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-import nest_asyncio
-
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="AI")
 st.title("Twitter Analysis")
 
-nest_asyncio.apply()
 c = twint.Config()
 
 st.write("Enter the keyword you want to search for")
