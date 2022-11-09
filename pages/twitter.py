36b6c4dacaa7a52f18693681ecd51dd22e9c1603
Леонид Каретников <poczta130@gmail.com>
2022-11-09 01:22:03 +0000

    requirements.txt

--- pages/twitter.py
+++ pages/twitter.py
@@ -1,6 +1,34 @@
 import streamlit as st
 from streamlit.components.v1 import iframe
 import pandas as pd
+import twint
+from collections import Counter
+
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="Diploma Generator")
-st.title("Report Generator")
+st.title("Twitter Analysis")
+
+
+# # https://github.com/twintproject/twint.git@origin/master#egg=twint
+
+#
+# # c = twint.Config()
+# # c.Search = '#blacklivesmatter'
+# # c.Limit = 2
+# # twint.run.Search(c)
+# #
+# # c.Pandas = True
+# # Tweets_df = twint.storage.panda.Tweets_df
+# # Tweets_df.to_csv('tweets.csv')
+#
+# #configuration
+# config = twint.Config()
+# config.Search = "bitcoin"
+# config.Lang = "en"
+# config.Limit = 100
+# # config.Since = "2019–04–29"
+# # config.Until = "2020–04–29"
+# config.Store_json = True
+# config.Output = "bitcoin_out.json"
+# #running search
+# twint.run.Search(config)
