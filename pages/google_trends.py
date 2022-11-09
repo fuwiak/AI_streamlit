79671500109baa418c759a129f23aab944451aa6
Леонид Каретников <poczta130@gmail.com>
2022-11-09 00:30:49 +0000

    a

--- pages/google_trends.py
+++ pages/google_trends.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit.components.v1 import iframe
 import pandas as pd
 # import plotly.express as px
-import googletrans
+# import googletrans
 from fpdf import FPDF
 import base64
 import pyautogui
